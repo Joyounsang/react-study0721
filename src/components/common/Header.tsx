efc3e8fe78db200d8237ea5bd9ca60dcaba6ba27
Joyounsang <younsang.jo@gmail.com>
2023-07-21 20:14:28 +0900

    mission 페이지 컴포넌트에서 중복된 코드를 찾아 별개의 컴포넌트로 세분화.

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,3 +1,48 @@
 export const Header = () => {
-  return <p></p>;
+  return (
+    <header className="header-wrap sub">
+      <div className="container-inner">
+        <h1 className="logo">
+          <a href="/">
+            <span>제59회 백상예술대상 Baeksang Arts Award</span>
+          </a>
+        </h1>
+        <div className="gnb">
+          <ul>
+            <li>
+              <a className="on" href="/about">
+                <span>시상식 소개</span>
+              </a>
+            </li>
+            <li>
+              <a className="" href="/awards">
+                <span>59회 수상자 · 작품</span>
+              </a>
+            </li>
+            <li>
+              <a className="" href="/replay">
+                <span>다시보기</span>
+              </a>
+            </li>
+            <li>
+              <a className="" href="/winners">
+                <span>역대 수상</span>
+              </a>
+            </li>
+            <li>
+              <a className="" href="/notice">
+                <span>공지사항</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="nav">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </header>
+  );
 };
